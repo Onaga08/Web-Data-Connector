dd7bd9afb6cf8245778ae19e034ecee4bec7a89a
Dhruv <pa8618@srmist.edu.in>
2024-04-05 21:44:30 +0530

    commit

--- my-wdc.js
+++ my-wdc.js
@@ -14,7 +14,14 @@ $(document).ready(function() {
         { id: "pollutant_id", dataType: tableau.dataTypeEnum.string },
         { id: "pollutant_min", dataType: tableau.dataTypeEnum.float },
         { id: "pollutant_max", dataType: tableau.dataTypeEnum.float },
-        { id: "pollutant_avg", dataType: tableau.dataTypeEnum.float }
+        { id: "pollutant_avg", dataType: tableau.dataTypeEnum.float },
+        // Add the required fields specified by Tableau
+        { id: "id", dataType: tableau.dataTypeEnum.string },
+        { id: "columns", dataType: tableau.dataTypeEnum.string },
+        { id: "alias", dataType: tableau.dataTypeEnum.string },
+        { id: "description", dataType: tableau.dataTypeEnum.string },
+        { id: "incrementColumnId", dataType: tableau.dataTypeEnum.string },
+        { id: "joinOnly", dataType: tableau.dataTypeEnum.bool }
     ];
 
     // Define the schema retrieval function
