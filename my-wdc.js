798eba0c65f6a6ac0ab54e3dd8ba9ffc0b825a89
Dhruv <pa8618@srmist.edu.in>
2024-04-05 18:58:19 +0530

    update

--- my-wdc.js
+++ my-wdc.js
@@ -22,7 +22,7 @@ $(document).ready(function() {
     // Define data retrieval function
     myConnector.getData = function (table, doneCallback) {
         // Retrieve data from API and format it
-        var apiEndpoint = "https://api.data.gov.in/resource/cbfeb319-56a4-4fb3-9d88-8cbae69e4662?api-key=579b464db66ec23bdd000001cdd3946e44ce4aad7209ff7b23ac571b&format=json";
+        var apiEndpoint = "https://api.data.gov.in/resource/3b01bcb8-0b14-4abf-b6f2-c1bfd384ba69?api-key=579b464db66ec23bdd000001cdd3946e44ce4aad7209ff7b23ac571b&format=json";
         
         // Make API request
         $.getJSON(apiEndpoint, function (data) {
