44fe4796543420107c9ddb61de375575243e69c9
Dhruv <pa8618@srmist.edu.in>
2024-04-05 20:42:38 +0530

    tweaks

--- my-wdc.js
+++ my-wdc.js
@@ -1,32 +1,62 @@
 $(document).ready(function() {
-    // Define connector
     var myConnector = tableau.makeConnector();
-
-    // Define schema
     myConnector.getSchema = function (schemaCallback) {
-        // Define schema here
+        
         var schema = [];
-        // Add fields to schema
+        //Schema
         schema.push({
-            id: "example_field_1",
+            id: "country",
             dataType: tableau.dataTypeEnum.string
         });
         schema.push({
-            id: "example_field_2",
+            id: "state",
+            dataType: tableau.dataTypeEnum.string
+        });
+        schema.push({
+            id: "city",
+            dataType: tableau.dataTypeEnum.string
+        });
+        schema.push({
+            id: "station",
+            dataType: tableau.dataTypeEnum.string
+        });
+        schema.push({
+            id: "last_update",
+            dataType: tableau.dataTypeEnum.datetime
+        });
+        schema.push({
+            id: "latitude",
             dataType: tableau.dataTypeEnum.float
         });
-        // Call schemaCallback with the schema
+        schema.push({
+            id: "longitude",
+            dataType: tableau.dataTypeEnum.float
+        });
+        schema.push({
+            id: "pollutant_id",
+            dataType: tableau.dataTypeEnum.string
+        });
+        schema.push({
+            id: "pollutant_min",
+            dataType: tableau.dataTypeEnum.float
+        });
+        schema.push({
+            id: "pollutant_max",
+            dataType: tableau.dataTypeEnum.float
+        });
+        schema.push({
+            id: "pollutant_avg",
+            dataType: tableau.dataTypeEnum.float
+        });
+        //Callback with the schema
         schemaCallback(schema);
     };
-
-    // Define data retrieval function
     myConnector.getData = function (table, doneCallback) {
-        // Retrieve data from API and format it
+       
         var apiEndpoint = "https://api.data.gov.in/resource/3b01bcb8-0b14-4abf-b6f2-c1bfd384ba69?api-key=579b464db66ec23bdd000001cdd3946e44ce4aad7209ff7b23ac571b&format=json";
-        
-        // Make API request
+
         $.getJSON(apiEndpoint, function (data) {
-            // Format data properly
+            // Format data properly!
             var formattedData = [];
             $.each(data.records, function (index, record) {
                 var formattedRecord = {
